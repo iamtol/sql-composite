22e76c348e5f6056924daacadecd71b534d59540
iamtol <peacekung@gmail.com>
2022-04-18 21:35:29 +0700

    adjust to test env.

--- CompositeJDBC.java
+++ CompositeJDBC.java
@@ -1,6 +1,9 @@
+import java.io.FileWriter;
 import java.sql.*;
 import cs.jdbc.driver.*;
 import cs.jdbc.driver.Connection;
+import java.io.IOException;
+
 
 public class CompositeJDBC 
 {
@@ -23,7 +26,7 @@ public class CompositeJDBC
         return conn;
     }
     
-    private void printHeaders(ResultSetMetaData rsmd, int colCount)
+/*    private void printHeaders(ResultSetMetaData rsmd, int colCount)
         throws SQLException
     {
         int lengths[] = new int[colCount + 1];
@@ -31,62 +34,81 @@ public class CompositeJDBC
         for (int i = 1; i <= colCount; i++) {
             String colName = rsmd.getColumnName(i);
             lengths[i] = colName.length();
-            System.out.print(colName + "\t");
+            System.out.print(colName + ",");
         }
         System.out.println();
-        
-        for (int i = 1; i <= colCount; i++) {
-            String dashes = "----------------".substring(0, lengths[i]);
-            System.out.print(dashes + "\t");
-        }
-        System.out.println();
-    }
+       
+    }*/
     
-    public void query(Connection conn, String table) 
-        throws SQLException
+    public void query(Connection conn, String table, String filename) 
+        throws SQLException, IOException
     {
         Statement stmt = null;
         ResultSet rs = null;
         ResultSetMetaData rsmd = null;
         
-        try {
+		try {
+            FileWriter fw = new FileWriter(filename + ".csv");
             stmt = conn.createStatement();
-            stmt.execute("SELECT top 300000 EN_FRST_NM, EN_SURNM FROM IP_SHR.VC_IP"
-            		+ "where IP_ST_CD = '1'"
-            		+ "AND VLD_TO_DT = '9999-12-31 23:59:59'"
-            		+ "AND (PRIM_SEG_CD = 'HN'"
-            		+ "OR DUAL_SEG_CD = 'HN')"
-            		+ "AND EN_FRST_NM IS NOT NULL"
+            stmt.execute("SELECT top 300000 EN_FRST_NM, EN_SURNM FROM IP_SHR.VC_IP\n"
+            		+ "where IP_ST_CD = '1'\n"
+            		+ "AND VLD_TO_DT = '9999-12-31 23:59:59'\n"
+            		+ "AND (PRIM_SEG_CD = 'HN'\n"
+            		+ "OR DUAL_SEG_CD = 'HN')\n"
+            		+ "AND EN_FRST_NM IS NOT NULL\n"
             		+ "AND EN_SURNM IS NOT NULL;");
-
+            		
+            //stmt.execute("SELECT * FROM IP_SHR.VC_IP;");
             rs = stmt.getResultSet();
             rsmd = rs.getMetaData();
             int colCount = rsmd.getColumnCount();
 
-            printHeaders(rsmd, colCount);
+            //printHeaders(rsmd, colCount);
 
-            while (rs.next()) {
+/*           while (rs.next()) {
                 for (int i = 1; i <= colCount; i++) {
-                    System.out.print(rs.getString(i) + "\t");
+                    System.out.print(rs.getString(i) + ",");
                 }
                 System.out.println();
+            }*/
+            
+            for(int i = 1; i <= colCount; i ++){
+                fw.append(rs.getMetaData().getColumnLabel(i));
+                if(i < colCount) fw.append(',');
+                else fw.append('\n');
+             }
+            
+            while (rs.next()) {
+
+                for(int i = 1; i <= colCount; i ++){
+                    fw.append(rs.getString(i).trim());
+                    if(i < colCount) fw.append(',');
+                }
+                fw.append('\n');
             }
-        }
+            fw.flush();
+            fw.close();
+		}
+        
         finally {
             if (rs   != null) rs.close();
             if (stmt != null) stmt.close();
             if (conn != null) conn.close();
+            
         }
+
     }
-    
-    public static void main(String args[]) 
+
+	public static void main(String args[]) 
     {
         Connection conn;
         CompositeJDBC composite = new CompositeJDBC();
         
         try {
-            conn = composite.connect("IP_SHR.VC_IP");
-            composite.query(conn, "DIH");
+        	System.out.println("Processing...");
+            conn = composite.connect("DIH");
+            composite.query(conn, "DIH", "outputCSV");
+            System.out.println("Completed");
         }
         catch (Exception ex) {
             ex.printStackTrace();
